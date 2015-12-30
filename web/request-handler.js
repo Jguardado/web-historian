04f8e75be376dddcbd7efabd53e20b2e13d4ee42
jguardado <jguardado85@gmail.com>
2015-12-30 12:36:05 -0800

    No test passing, working on readFile

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,45 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helper = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //console.log("\n\n\n\n\n", req.method);
+
+  if (req.method === "GET"){
+  //console.log(res);
+
+  //console.log('PATHS STRING:', archive.paths.siteAssets + "/index.html");
+  
+
+  console.log(res);
+  fs.readFile("archive.paths.siteAssets + 'index.html' ", function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+  res.writeHead(200, helper.headers);
+  res.write(html);
+  res.end();
+
+
+
+  // res.end(fs.readFile('archive.paths.siteAssets + "/index.html" '));
+
+  //   http.createServer(function(request, response) {  
+  //       response.writeHeader(200, {"Content-Type": "text/html"});  
+  //       response.write(html);  
+  //       response.end();  
+  //   }).listen(8000);
+});
+
+//   fs.readFile('/etc/passwd', function (err, data) {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+  }
+  
+    res.end(archive.paths.list);
+  
 };
